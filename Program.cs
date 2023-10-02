e91510beb531d758db466997a3758fcdf29b4c8f
Jeshua Herrera Bedoya <Jeshuah71>
2023-10-01 21:54:39 -0600

    PracticeCh14

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Chapter 14 practice
+int a = 10; //Declare a variable "a" on stack
+int b = 5; //Declare a variable "b" on stack
+int result = Add(a , b); // Call add function to add
+Console.WriteLine("Result "  + result);
+
+int Add(int x , int y )
+{
+    //The x and y values are allocated in this stack within this function
+    return x + y; // the result is calculated in the stack
+}
+
+// Heap example 
+Console.WriteLine("What is your name? ");
+string name = Console.ReadLine();
+Greeting(name);
+
+void Greeting(string name)
+{
+    Console.WriteLine("Hi " + name );
+
+}
